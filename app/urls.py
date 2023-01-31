15d750cc7fc5bd7f849fbe5ce4a05cc1d5c1b513
unknown <carlos_david753@hotmail.com>
2023-01-31 11:17:33 -0500

    segundo montaje

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('home.urls')),
+    url(r'^Blog/', include('blog.urls')),
+    url(r'^Videos/', include('video.urls')),
+    url(r'^Courses/', include('courses.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,5 @@ urlpatterns=[
 		url(r'^$',Index,name="Index"),
 		url(r'About/$',About,name="About"),
 		url(r'Category/$',Category,name="Category"),
-		url(r'Blog/$',Blog,name="Blog"),
-		url(r'Blog_Details/$',Blog_Details,name="Blog_Details"),
 		url(r'Contact/$',Contact,name="Contact"),
 	]
